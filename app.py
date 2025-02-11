66516c3fa0db864d90d4eea1dc91d8768e8174ae
qoeka98 <vhzkflfltm6@gmail.com>
2025-02-11 11:02:27 +0900

    １

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-
-
 import streamlit as st
 from eda import run_eda
 from home import run_home
 from ml import run_ml
 
 # 🔹 **사이드바 메뉴 추가**
-menu_option = st.sidebar.radio("메뉴 선택", ["🏠 홈", "🎮 게임",'앱개발과정'])
+st.sidebar.title("📌 메뉴 선택")
+menu_option = st.sidebar.radio("메뉴를 선택하세요", ["🏠 홈", "🎮 게임", "📚 앱 개발 과정"])
 
 if menu_option == "🏠 홈":
     run_home()
 
 elif menu_option == "🎮 게임":
-    run_eda()  
+    run_eda()
 
-elif menu_option == '앱개발과정':
+elif menu_option == "📚 앱 개발 과정":
     run_ml()
