7c0f0a0ac88088b6e2cccd9559e1c2271b253bef
qoeka98 <vhzkflfltm6@gmail.com>
2025-02-11 20:19:14 +0900

    1

--- home.py
+++ home.py
@@ -1,9 +1,5 @@
-import gradio as gr
+import streamlit as st
 
 def run_home():
-    with gr.Blocks() as home_ui:
-        gr.Markdown("## 가위바위보 몬스터 배틀")
-        gr.Markdown("Gradio 기반으로 실행되는 가위바위보 게임 소개 페이지입니다!")
-        gr.Image(value="./image/게임.png", label="게임 이미지")
-
-    return home_ui
+    pass
+    
