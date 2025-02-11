5fe3cda8b207707f3d7bafb5bdf9f3601163f943
qoeka98 <vhzkflfltm6@gmail.com>
2025-02-11 12:22:42 +0900

    １

--- game.py
+++ game.py
@@ -62,7 +62,10 @@ def run_game():
         async_transform=True,
         rtc_configuration={"iceServers": [
             {"urls": ["stun:stun.l.google.com:19302"]},
-            {"urls": ["stun:stun1.l.google.com:19302"]}
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun3.l.google.com:19302"]},
+            {"urls": ["stun:stun4.l.google.com:19302"]}
         ]}
     )
 
